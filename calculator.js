954b70bc36225faab8335ac5fa7077a9ca9b263a
oOFaYOo <nika.tseleva@mail.ru>
2020-08-08 01:20:50 +0500

    Начала писать функцию "равно", joinNumbers

--- calculator.js
+++ calculator.js
@@ -1,33 +1,56 @@
+// let output = []; //То, что выводится в дисплей
+// let input = document.getElementsByClassName("button"); //То, что вводится с клавиатуры калькулятора
+// let buttonEquals = document.querySelector(".equals"); //Кнопка "равно"
+//
+// for (let part of input) {
+//     part.onclick = () => {  //При нажатии на копошки...
+//         if (output.length === 0 && isNaN(part.innerHTML) && part.innerHTML !== "-") {  //Позваляет начать строку с цифры или со знака минус
+//             return;
+//         }
+//
+//         function point(output) {  //Отображение точки
+//             let outputToString = output.join("");
+//             let outputToArr = outputToString.split("+" || "-" || "x" || "÷");
+//             return outputToArr[outputToArr.length - 1].includes(".");
+//         }
+//
+//         if (part.innerHTML === "." && (point(output))) {
+//             return;
+//         }
+//         if (isNaN(output[output.length - 1]) && part.classList.contains("sign")) { //Не дает писать два знака подряд
+//             output.pop();
+//         }
+//         output.push(part.innerHTML); //Сохраняем значения в "вывод на дисплей"
+//         document.getElementById("input").innerHTML = output.join(""); //Выводим
+//     }
+// }
+//
+// document.getElementById("clean").onclick = () => {    //Очистка дисплея
+//     document.getElementById("input").innerHTML = null;
+//     output = [];
+// };
 
-let output = []; //То, что выводится в дисплей
-let input = document.getElementsByClassName("button"); //То, что вводится с клавиатуры калькулятора
 
+// buttonEquals.onclick = equals (output);
 
-for (let part of input) {
-    part.onclick = () => {  //При нажатии на копошки...
-        if (output.length === 0 && isNaN(part.innerHTML) && part.innerHTML !== "-") {
-            return;
-        }
-        function point(output) {
-            let output2 = output.join("");
-            let output3 = output2.split("+" || "-" || "x" || "÷");
-            return output3[output3.length-1].includes(".");
-        }
-        if (part.innerHTML === "." && (point(output))){
-            return;
-        }
-        if (isNaN(output[output.length - 1]) && part.classList.contains("sign")) { //Не дает писать два знака подряд
-            output.pop();
-        }
-        output.push(part.innerHTML); //Сохраняем значения в "вывод на дисплей"
-        document.getElementById("input").innerHTML = output.join(""); //Выводим
-    }
+function equals(output){
+    output = joinNumbers(output);
+    output = convertToRPN(output);
+
 }
 
-document.getElementById("clean").onclick = () => {    //Очистка дисплея
-    document.getElementById("input").innerHTML = null;
-    output = [];
-};
+function joinNumbers(output) {
+    let outputToString = output.join("");
+    let outputString = "";
+    for (let char of outputToString) {
+        if (char === "x" || "+" || "-" || "÷") {
+            char = " " + `${char}` + " ";
+        }
+        outputString = outputString + char;
+    }
+    return outputString.split(" ");
+}
 
+function convertToRPN(output) {
 
-
+}
