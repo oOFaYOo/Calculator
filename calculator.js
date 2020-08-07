edf87e8808adb83cd71782804c45266072ff91fd
oOFaYOo <nika.tseleva@mail.ru>
2020-08-08 03:13:37 +0500

    Исправление ошибок

--- calculator.js
+++ calculator.js
@@ -1,37 +1,44 @@
-// let output = []; //То, что выводится в дисплей
-// let input = document.getElementsByClassName("button"); //То, что вводится с клавиатуры калькулятора
-// let buttonEquals = document.querySelector(".equals"); //Кнопка "равно"
-//
-// for (let part of input) {
-//     part.onclick = () => {  //При нажатии на копошки...
-//         if (output.length === 0 && isNaN(part.innerHTML) && part.innerHTML !== "-") {  //Позваляет начать строку с цифры или со знака минус
-//             return;
-//         }
-//
-//         function point(output) {  //Отображение точки
-//             let outputToString = output.join("");
-//             let outputToArr = outputToString.split("+" || "-" || "x" || "÷");
-//             return outputToArr[outputToArr.length - 1].includes(".");
-//         }
-//
-//         if (part.innerHTML === "." && (point(output))) {
-//             return;
-//         }
-//         if (isNaN(output[output.length - 1]) && part.classList.contains("sign")) { //Не дает писать два знака подряд
-//             output.pop();
-//         }
-//         output.push(part.innerHTML); //Сохраняем значения в "вывод на дисплей"
-//         document.getElementById("input").innerHTML = output.join(""); //Выводим
-//     }
-// }
-//
-// document.getElementById("clean").onclick = () => {    //Очистка дисплея
-//     document.getElementById("input").innerHTML = null;
-//     output = [];
-// };
+let output = []; //То, что выводится в дисплей
+let input = document.getElementsByClassName("button"); //То, что вводится с клавиатуры калькулятора
+let buttonEquals = document.querySelector(".equals"); //Кнопка "равно"
+
+for (let part of input) {
+    part.onclick = () => {  //При нажатии на копошки...
+        if (output.length === 0 && isNaN(part.innerHTML) && part.innerHTML !== "-") {  //Позваляет начать строку с цифры или со знака минус
+            return;
+        }
+
+        function point(output) {  //Отображение точки
+            let outputToString = output.join("");
+            let outputString = "";
+            for (let char of outputToString) {
+                if (char === "x" || char === "+" || char === "-" || char === "÷") {
+                    char = " ";
+                }
+                outputString = outputString + char;
+            }
+            output = outputString.split(" ");
+            return output[output.length - 1].includes(".");
+        }
+
+        if (part.innerHTML === "." && (point(output))) {
+            return;
+        }
+        if (isNaN(output[output.length - 1]) && part.classList.contains("sign")) { //Не дает писать два знака подряд
+            output.pop();
+        }
+        output.push(part.innerHTML); //Сохраняем значения в "вывод на дисплей"
+        document.getElementById("input").innerHTML = output.join(""); //Выводим
+    }
+}
+
+document.getElementById("clean").onclick = () => {    //Очистка дисплея
+    document.getElementById("input").innerHTML = null;
+    output = [];
+};
 
 
-// buttonEquals.onclick = equals (output);
+buttonEquals.onclick = equals (output);
 
 function equals(output){
     output = joinNumbers(output);
@@ -43,7 +50,7 @@ function joinNumbers(output) {
     let outputToString = output.join("");
     let outputString = "";
     for (let char of outputToString) {
-        if (char === "x" || "+" || "-" || "÷") {
+        if (char === "x" || char === "+" || char === "-" || char === "÷") {
             char = " " + `${char}` + " ";
         }
         outputString = outputString + char;
@@ -53,7 +60,7 @@ function joinNumbers(output) {
 
 function convertToRPN(output) {
     for(let i = 0; i < output.length; i++){
-        if(output[i] === "x" || "÷" && output[i + 1]!== "+" || "-" || "x" || "÷"){
+        if(output[i] === "x" || "÷" && output[i + 1]!== "+" || output[i + 1]!== "-" || output[i + 1]!== "x" || output[i + 1]!== "÷"){
             let value = output[i];
             output[i] = output[i + 1];
             output[i + 1] = value;
@@ -68,8 +75,8 @@ function convertToRPN(output) {
         outputString = outputString + char;
     }
     output = outputString.split(" ");
-    for(let i = 0; i < output.length; i++){
-        if(output[i] === "-" || "+" && output[i + 1]!== "+" || "-"){
+    for(let i = 0; i < output.length-1; i++){
+        if(output[i] === "-" || "+" && output[i + 1]!== "+" || output[i + 1]!== "-"){
             let value = output[i];
             output[i] = output[i + 1];
             output[i + 1] = value;
